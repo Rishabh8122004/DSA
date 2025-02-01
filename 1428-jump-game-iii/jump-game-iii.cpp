89472dd41a8c481f2ae27b38f01e93dc003caad5
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-02-01 12:02:24 +0530

    Time: 3 ms (48.67%) | Memory: 39.7 MB (29.87%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool solve(vector<int>& arr, int start, vector<bool>& v) {
-        if (start > arr.size() - 1 || start < 0)
+        if (start > arr.size() - 1 || start < 0) // Out of bounds
             return false;
-        if (arr[start] == 0)
+        if (arr[start] == 0) // Found target
             return true;
-        if(v[start]) return false;
-        v[start] = true;
-        if (solve(arr, start + arr[start],v))
-            return true;
-        if (solve(arr, start - arr[start],v))
-            return true;
-        return false;
+        if (v[start]) return false; // Already visited, avoid cycles
+
+        v[start] = true; // Mark as visited
+
+        // Try jumping forward and backward
+        return solve(arr, start + arr[start], v) || solve(arr, start - arr[start], v);
     }
+
     bool canReach(vector<int>& arr, int start) {
-        vector<bool> v(arr.size(),false);
+        vector<bool> v(arr.size(), false); // Visited array
         return solve(arr, start, v);
     }
 };
