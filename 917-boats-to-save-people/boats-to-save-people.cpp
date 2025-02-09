17fd343a762a62db98bdeec2ec918751c3eb45bf
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-02-09 10:36:50 +0530

    Time: 1330 ms (6.18%) | Memory: 48.6 MB (5.08%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int n = people.size();
+        vector<int> freq(limit+1);
+        int ans = 0;
+        for(int j = 0;j<=n-1;j++){
+            freq[people[j]]++;
+        }
+        for(int i = 0;i<n;i++){
+            if(freq[people[i]]>0){
+            if(freq[limit-people[i]]>0){freq[limit-people[i]]--;}
+            else{
+                for(int j = n-1;j>i;j--){
+                    if((freq[people[j]]>0) &&(people[i]+people[j]<=limit)){freq[people[j]]--;break;}
+                }
+            }
+            ans++;
+            freq[people[i]]--;}
+        }
+        return ans;
+    }
+};
