6c0aeba0adcd506d669317f563ec995a0ccdd78e
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-02-09 14:57:11 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (68.95%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.cpp
+++ 2722-prime-in-diagonal/prime-in-diagonal.cpp
@@ -5,12 +5,14 @@ public:
         int ans = 0;
         for(int i = 0;i<n;i++){
             bool flag = false;
-            for(int j = 2;j<=sqrt(nums[i][i]);j++){
+            int a = sqrt(nums[i][i]); 
+            for(int j = 2;j<=a;j++){
                 if(nums[i][i]%j == 0){flag = true;break;}
             }
             if(!flag) ans = max(ans,nums[i][i]);
             flag = false;
-            for(int j = 2;j<=sqrt(nums[i][n-1-i]);j++){
+            a = sqrt(nums[i][n-1-i]); 
+            for(int j = 2;j<=a;j++){
                 if(nums[i][n-1-i]%j == 0){flag = true;break;}
             } 
             if(!flag)ans = max(ans,nums[i][n-1-i]);  
