28b4821fd0ae332f2c4216f1739414a19b3254b4
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-02-10 09:35:10 +0530

    Time: 94 ms (9.80%) | Memory: 39.3 MB (5.48%) - LeetSync

---
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isprime(int x){
+        if(x == 1)return 0;
+        int a = sqrt(x);
+        for(int i = 2;i<=a;i++){
+            if(x%i == 0)return 0;
+        }
+        return 1;
+    }
+    void factorisprime(int x,vector<int> &prime){
+        vector<int>v;
+        int a = sqrt(x);
+        for(int i = 2;i<=a;i++){
+            if(x%i == 0){
+                v.push_back(i);
+                v.push_back(x/i);
+            }
+        }
+        for(int i = 0;i<v.size();i++){
+            if(isprime(v[i])){
+                bool flag = true;
+                for(int j = 0;j<prime.size();j++){
+                    if(v[i] == prime[j]){flag = false;break;}
+                }
+                if(flag) prime.push_back(v[i]);
+            }
+        }
+        return;
+    }
+    int distinctPrimeFactors(vector<int>& nums) {
+        vector<int> prime;
+        for(int i = 0;i<nums.size();i++){
+            if(isprime(nums[i])){
+                bool flag = true;
+                for(int j = 0;j<prime.size();j++){
+                    if(nums[i] == prime[j]){flag = false;break;}
+                }
+                if(flag) prime.push_back(nums[i]);
+            }
+            else{
+                factorisprime(nums[i],prime);
+            }
+        }
+        return prime.size();
+    }
+};
