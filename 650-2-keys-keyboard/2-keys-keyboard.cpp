51a30ce0d6242bfc5e8dcfef02b09cd3ecedfd36
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-02-11 08:57:51 +0530

    Time: 2 ms (55.68%) | Memory: 7.9 MB (57.28%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isprime(int x){
+        if(x == 1)return 0;
+        for(int i = 2;i<=sqrt(x);i++){
+            if(x%i == 0)return 0;
+        }
+        return 1;
+    }
+    int minSteps(int n) {
+        if(n == 1) return 0;
+        if(isprime(n))return n;
+        int steps = 0;
+        // we have to find the minimum prime factor of the number n 
+        int minsum = n;
+        for(int i = 2;i<=sqrt(n);i++){
+            if(n%i == 0){
+                if(minsum>(i+minSteps(n/i))){minsum = i+minSteps(n/i);}
+            }
+        }
+        return minsum;
+    }
+};
