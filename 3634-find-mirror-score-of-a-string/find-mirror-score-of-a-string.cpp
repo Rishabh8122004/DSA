1ddce0918b11ea1a8f4a6cb08f8186fe5cbff3d2
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-03-04 09:26:51 +0530

    Time: 22 ms (76.75%) | Memory: 18 MB (51.47%) - LeetSync

--- 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
+++ 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
@@ -4,15 +4,15 @@ public:
         int n = s.size();
         long long int ans = 0;
         vector<vector<int>>v;
-        for(int i = 0;i<26;i++)v.push_back({-1});
-        bool flag = true;
+        for(int i = 0;i<26;i++)v.push_back({-1}); // SETTING ALL THE 26 VECTORS WITH -1 AS THEIR BEGGINER ELEMENT TO AVOID SOME ISSUES
+        bool flag = true; // FLAG TO TELL US THAT CURRENT ELEMENT IS OKAY TO PUSH BACK OR NOT ?
         for(int i = 1;i<n;i++){
-            if(flag) v[s[i-1]-'a'].push_back(i-1);
+            if(flag) v[s[i-1]-'a'].push_back(i-1); // IF PREVIOUS ELEMENT IS'NT USED EARLIER AS A MIRROR ELEMENT THEN PUSH_BACK IT .
             flag = true;
-            if(v['z'-s[i]].size()>1){
-                ans+=(i-v['z'-s[i]][v['z'-s[i]].size()-1]);
-                v['z'-s[i]].pop_back();
-                flag = false;
+            if(v['z'-s[i]].size()>1){//IS ANY MIRROR ELEMENT AVAILABLE TILL CURRENT INDEX ?
+                ans+=(i-v['z'-s[i]][v['z'-s[i]].size()-1]); // IF YES, THEN WE GOTTA UPDATE THE ANSWER
+                v['z'-s[i]].pop_back(); // REMOVING THE USED MIRROR ELEMENT'S INDEX
+                flag = false;/// MARKING THE CURRENT ELEMENT AS MARKED TO AVOID USING IT IN THE NEXT STEP.
             }
         }
         return ans;
