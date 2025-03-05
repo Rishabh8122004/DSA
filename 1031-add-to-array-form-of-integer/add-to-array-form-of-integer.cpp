2b26a659225d6b9675e46ddf51503ea7ae6b6281
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-03-05 09:20:36 +0530

    Time: 4 ms (18.00%) | Memory: 32.9 MB (21.87%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        vector<int>v;
-        int i = num.size()-1;
-        while(k>0 || i>=0){
-            if(k>0 && i>=0){
-            int x = (k%10 + num[i]);
-            k/=10;
-            if(x>=10){
-                k++;
-                x%=10;
-            }
-            v.push_back(x);
-            i--;
-            }
-            else if(k == 0 && i>=0){
+        vector<int> v;
+        int i = num.size() - 1;
+        while (k > 0 || i >= 0) {
+            if (k > 0 && i >= 0) {
+                int x = (k % 10 + num[i]);
+                k /= 10;
+                if (x >= 10) {
+                    k++;
+                    x %= 10;
+                }
+                v.push_back(x);
+                i--;
+            } else if (k == 0 && i >= 0) {
                 v.push_back(num[i]);
                 i--;
-            }
-            else if(i<0 && k>0){
-                v.push_back(k%10);
-                k/=10;
+            } else if (i < 0 && k > 0) {
+                v.push_back(k % 10);
+                k /= 10;
             }
         }
         i = 0;
-        int j = v.size()-1;
-        while(i<j){
-            swap(v[i],v[j]);
-            i++;j--;
+        int j = v.size() - 1;
+        while (i < j) {
+            swap(v[i], v[j]);
+            i++;
+            j--;
         }
         return v;
     }
