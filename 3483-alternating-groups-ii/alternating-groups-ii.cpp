5c1af714b013fb0d4b3b9d43f9d2deaf4062cb4c
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-03-09 23:53:52 +0530

    Time: 100 ms (7.32%) | Memory: 103.8 MB (42.05%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n = colors.size();
+        vector<int>v;
+        for(int i = 1;i<n;i++){
+        if(colors[i] == colors[i-1]){
+            int j = i;
+            while(j<n){v.push_back(colors[j]);j++;}
+            j = 0;
+            while(j<i){v.push_back(colors[j]);j++;}
+            break;
+        }
+        }
+        if(v.size() == n){
+            for(int i = 0;i<n;i++){
+                colors[i] = v[i];
+                cout<<colors[i]<<" ";
+            }
+        }
+        int ans = 0;
+        int idx = -1;
+        for(int i = 1;i<k;i++){
+            if(colors[i] == colors[i-1]){idx = i-1;}
+        }
+        if(idx==-1)ans++;
+        int i = 1,j = k;
+        while(i<n){
+            if(colors[j%n] == colors[(j-1)%n])idx = (j-1)%n;
+            if(idx<i)ans++;
+            if(idx<i && idx != -1 &&j%n<i)ans--;
+            j++;i++;
+        }
+        return ans;
+    }
+};
