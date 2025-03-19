a0fecb9efdb0c3aa4cf53a60b9619acd6255b67a
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-03-19 07:46:20 +0530

    Time: 3 ms (89.05%) | Memory: 139.9 MB (93.66%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    void flip(vector<int>& nums,int i,int j){
-        while(i<=j){
-            nums[i] = (nums[i] == 0?1:0);
-            i++;
-        }
-        return;
-    }
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
-        int i = 0,j = 2;
-        while(i<n){
+        int n = nums.size(),ans = 0;
+        for(int i = 0;i < n;i ++){
             if(nums[i] == 0){
-                if(j>=n)return -1;
-              flip(nums,i,j);ans++;
+                ans ++;
+                if(i + 1 >= n || i + 2 >= n) return -1;
+                nums[i + 1] = 1 - nums[i + 1];
+                nums[i + 2] = 1 - nums[i + 2];
             }
-            i++;j++;
         }
-        //if(nums[n-1] != nums[n-2] || nums[n-1]!=nums[n-3])return -1;
+
         return ans;
     }
 };
