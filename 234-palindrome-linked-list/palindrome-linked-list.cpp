69cbbb620859c6b10c14d7acf0dac340c8ccb199
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-03-19 23:06:12 +0530

    Time: 9 ms (20.11%) | Memory: 123.5 MB (45.37%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,12 +11,12 @@
 class Solution {
 public:
     bool checking_using_recursion(ListNode* s,ListNode* &head){
-        if(s->next == NULL){return s->val == head->val;}
+        if(s->next == NULL){return s->val == head->val;} // if s is the last node, returning if it's equal to first node or not
         if(checking_using_recursion(s->next,head)){
-            head = head->next;
+            head = head->next; // if yes, moving head forward for checking if it's equal to current node or not
         }
-        else return false;
-        return s->val == head->val;
+        else return false; 
+        return s->val == head->val; // returning if values are equal or not
     }
     bool isPalindrome(ListNode* head) {
         ListNode* s = head,*f = head;
@@ -26,7 +26,7 @@ public:
             if(f == NULL)break;
             f = f->next;
         }
-        //s is the right middle node, now watch this.
+        //now, s is the right middle node. or the first node of the second half
         return checking_using_recursion(s,head);
     }
 };
