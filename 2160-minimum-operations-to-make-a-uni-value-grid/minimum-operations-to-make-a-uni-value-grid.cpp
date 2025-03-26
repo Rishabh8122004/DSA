1485caa1016217391cd4400c503f99708b57112b
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-03-26 08:24:40 +0530

    Time: 2307 ms (5.47%) | Memory: 92.2 MB (17.32%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int ans = INT_MAX;
-        vector<int> v;
+        int ans = INT_MIN;
         for(int i = 0;i<grid.size();i++){
             for(int j = 0;j<grid[i].size();j++){
-                v.push_back(grid[i][j]);
+                ans = max(ans,grid[i][j]);
             }
         }
-        sort(v.begin(),v.end());
-        for(int i = v.size()/2;i<v.size() && i <= (v.size()/2)+1;i++){
-            int a = 0;
-            bool flag = true;
-            for(int j = 0;j<v.size();j++){
-                int d = v[i]-v[j];
+        vector<pair<int,bool>> v(ans+1,{0,false});
+        for(int i = 0;i<grid.size();i++){
+            for(int j = 0;j<grid[i].size();j++){
+                v[grid[i][j]].second = true;
+                v[grid[i][j]].first++;
+
+            }
+        }
+        ans = INT_MAX;
+        vector<int> ele;
+        for(int i = 0;i<v.size();i++){if(v[i].second){while(v[i].first--){ele.push_back(i);cout<<i<<" ";}}}
+        for(int i = ele.size()/2;i<ele.size() && i <= (ele.size()/2)+1;i++){
+            int a = 0;bool flag = true;
+            for(int j = 0;j<grid.size();j++){
+                for(int k = 0;k<grid[j].size();k++){
+                int d = ele[i]-grid[j][k];
                 if(d%x != 0){flag = false;break;}
                 d = abs(d);
                 a+=(d/x);
+                }
+                if(!flag)break;
             }
             if(flag)ans = min(a,ans);
         }
