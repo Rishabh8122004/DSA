c620bea7a3e882f64b74d8762ad68d94d8dce4ea
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-03-26 08:27:13 +0530

    Time: 29 ms (82.98%) | Memory: 92.1 MB (17.32%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -18,7 +18,7 @@ public:
         ans = INT_MAX;
         vector<int> ele;
         for(int i = 0;i<v.size();i++){if(v[i].second){while(v[i].first--){ele.push_back(i);}}}
-        for(int i = ele.size()/2;i<ele.size() && i <= (ele.size()/2)+1;i++){
+        for(int i = ele.size()/2;i<ele.size() && i <= (ele.size()/2);i++){
             int a = 0;bool flag = true;
             for(int j = 0;j<grid.size();j++){
                 for(int k = 0;k<grid[j].size();k++){
