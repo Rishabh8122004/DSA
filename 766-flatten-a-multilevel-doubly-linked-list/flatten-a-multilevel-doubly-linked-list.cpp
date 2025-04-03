0c8b054356f31fa4dbba7683523fa1a80fbcfa21
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-04-03 12:37:54 +0530

    Time: 7 ms (16.38%) | Memory: 11.6 MB (94.91%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -1,37 +1,33 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* prev;
-    Node* next;
-    Node* child;
-};
-*/
 class Solution {
 public:
     Node* flatten(Node* head) {
-        Node *b = head,*f = (b)?head->next:NULL;
-        while(b){
-            if(b->child!=NULL){
-                b->next = b->child;
-                b->child->prev = b;
-                Node *t = b->child;
-                while(t->next){
-                    t = t->next;
+        // `curr` is used to traverse the main list
+        Node *curr = head, *nextNode = (curr) ? head->next : NULL;
+
+        while (curr) {
+            // If the current node has a child, we merge it into the main list
+            if (curr->child != NULL) {
+                curr->next = curr->child;  // Link current node to its child
+                curr->child->prev = curr;  // Set child's previous pointer
+                
+                // Find the last node of the child list
+                Node *lastChild = curr->child;
+                while (lastChild->next) {
+                    lastChild = lastChild->next;
                 }
-                t->next = f;
-                if(f)f->prev = t;
-                f = b->next;
+
+                // Connect last node of the child list to `nextNode`
+                lastChild->next = nextNode;
+                if (nextNode) nextNode->prev = lastChild;
+
+                // Move `nextNode` to the new next node after merging
+                nextNode = curr->next;
             }
-            b = b->next;
-            if(f)f = f->next;
+            curr->child = NULL; // Flattened list should not have child pointers
+            // Move to the next node in the main list
+            curr = curr->next;
+            if (nextNode) nextNode = nextNode->next;
         }
-        b = head;
-        while(b){
-           b->child = NULL;
-           b = b->next;
-        }
-        return head;
+        return head; // Return the modified flattened list
     }
 };
