6e876cb6d4bf0462124a5c1ff02fde84eeef4430
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-04-10 19:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (62.35%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -6,9 +6,12 @@ public:
        for(int i = 1;i<s.size();i++){
         if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
         else{
-            if(!st.empty() && s[i] == ')' && st.top() == '(') st.pop();
-            else if(!st.empty() && s[i] == '}' && st.top() == '{') st.pop();
-            else if(!st.empty() && s[i] == ']' && st.top()== '[') st.pop();
+            if(!st.empty()){
+            if(s[i] == ')' && st.top() == '(') st.pop();
+            else if( s[i] == '}' && st.top() == '{') st.pop();
+            else if(s[i] == ']' && st.top()== '[') st.pop();
+            else return false;
+            }
             else return false;
         }
        }
