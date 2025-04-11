ef817a23a958596734c5bca371a469cd0f24781e
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-04-11 19:39:01 +0530

    Time: 63 ms (72.77%) | Memory: 9.6 MB (84.15%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-    bool issymmetric(int x){
-        vector<int>v;
-        while(x){
-            v.push_back(x%10);
-            x/=10;
-        }
+    bool issymmetric(string s){
+        int i = 0;
         int s1 = 0;
-        for(int i = 0;i<v.size();i++){
-            if(i<v.size()/2){s1+=v[i];}
-            else s1-=v[i];
-        }
+        while(i<s.size()/2){s1+=(s[i]-'a');i++;}
+        while(i<s.size()){s1-=(s[i]-'a');i++;}
         return s1 == 0;
     }
     int countSymmetricIntegers(int low, int high) {
@@ -18,7 +12,7 @@ public:
         for(int i = low;i<=high;i++){
             if(i>=1 && i<10){i = 9;continue;} 
             else if(i>=100 && i<1000){i = 999;continue;} 
-            if(issymmetric(i))ans++;
+            if(issymmetric(to_string(i)))ans++;
         }
         return ans;
     }
