a43f34cb58878aa1cefd57f96ea0c9043cbdaad2
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-04-17 17:30:22 +0530

    Time: 15 ms (5.36%) | Memory: 13.3 MB (11.82%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,17 +4,22 @@ public:
         stack<int>st;
         st.push(-1);
         st.push(n2[n2.size()-1]);
-        vector<int>v(n2.size());
-        v.back() = -1;
+        vector<vector<int>>v(n2.size());
+        v.back() = {n2[n2.size()-1],-1};
         for(int i = n2.size()-2;i>=0;i--){
             while(st.size()>1&&st.top()<=n2[i])st.pop();
-            v[i] = st.top();
+            v[i] = {n2[i],st.top()};
             st.push(n2[i]);
         }
+        sort(v.begin(),v.end());
         vector<int>ans;
         for(int i = 0;i<n1.size();i++){
-            for(int j = 0;j<n2.size();j++){
-                if(n1[i] == n2[j]){ans.push_back(v[j]);break;}
+            int lo = 0,hi = v.size()-1;
+            while(lo<=hi){
+                int mid = (lo+hi)/2;
+                if(v[mid][0] == n1[i]){ans.push_back(v[mid][1]);break;}
+                else if(v[mid][0]<n1[i]){lo++;}
+                else hi--;
             }
         }
         return ans;
