2d183445bd15a813cce21687da737e75e7324dab
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-04-22 10:21:41 +0530

    Time: 31 ms (5.63%) | Memory: 88.6 MB (81.94%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& h) {
         int n = h.size();
-        vector<int>nge(n);
+        vector<int>ans(n,0);
         stack<int>st;
         st.push(-1);
-        nge[n-1] = 0;
         for(int i = n-2;i>=0;i--){
-            int c = 0;
+            int c = 0;// number of pops performed for this number is the number of people it can see
             st.push(i+1);
             while(st.size()>1 && h[i]>h[st.top()]){st.pop();c++;}
-            nge[i] = c;
-            if(st.size()>1)nge[i]++;
+            ans[i] = c;
+            if(st.size()>1)ans[i]++; // if any number is greater than it,it is also be seen 
         }
-        return nge;
+        return ans;
     }
 };
