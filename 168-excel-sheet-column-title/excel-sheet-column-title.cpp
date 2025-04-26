3417aff103919a09bdb48cd036c47de11859266f
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-04-26 19:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (89.52%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     string convertToTitle(int cn) {
         string ans = "";
-        while(cn>0){
-            ans = (char)(((cn-1)%26)+'A') + ans;
-            cn--;
-            cn/=26;
+        while(cn > 0) {
+            ans = (char)((--cn % 26) + 'A') + ans;
+            cn /= 26;
         }
         return ans;
     }
