d507e82fdd3a6316d73b57d45ffdb77496ba9fd3
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-06 20:32:53 +0530

    Time: 78 ms (30.20%) | Memory: 45.2 MB (11.88%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -4,18 +4,18 @@ public:
         int n = s.size();
         stack<char>t;
         string p = "";
-        vector<vector<int>> v(26);
+        vector<deque<int>> v(26);
         for (int i = 0; i < n; i++) {
             v[s[i] - 'a'].push_back(i);
         }
         int j = 0;
         for(int i = 0;i<26;i++){
             if(v[i].empty())continue;
-            while(!v[i].empty() && j>v[i][0]){v[i].erase(v[i].begin());}
+            while(!v[i].empty() && j>v[i][0]){v[i].pop_front();}
             if(v[i].empty())continue;
             while(!t.empty() && t.top()<=('a'+i)){p+=t.top();t.pop();}
             while(!v[i].empty()){
-                if(j>v[i][0]){v[i].erase(v[i].begin());continue;}
+                if(j>v[i][0]){v[i].pop_front();continue;}
                 while(j<=v[i][0]){
                     t.push(s[j]);
                     j++;
