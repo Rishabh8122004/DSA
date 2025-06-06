b6a33a935ba17620764c6246977e327bd46b7f2f
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-06 20:35:32 +0530

    Time: 89 ms (25.25%) | Memory: 41.8 MB (14.36%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -2,30 +2,43 @@ class Solution {
 public:
     string robotWithString(string s) {
         int n = s.size();
-        stack<char>t;
+        stack<char> t;
         string p = "";
-        p.reserve(n);
+        p.reserve(n); // size is fixed, saves time..
         vector<deque<int>> v(26);
         for (int i = 0; i < n; i++) {
             v[s[i] - 'a'].push_back(i);
         }
         int j = 0;
-        for(int i = 0;i<26;i++){
-            if(v[i].empty())continue;
-            while(!v[i].empty() && j>v[i][0]){v[i].pop_front();}
-            if(v[i].empty())continue;
-            while(!t.empty() && t.top()<=('a'+i)){p+=t.top();t.pop();}
-            while(!v[i].empty()){
-                if(j>v[i][0]){v[i].pop_front();continue;}
-                while(j<=v[i][0]){
+        for (int i = 0; i < 26; i++) {
+            if (v[i].empty())
+                continue;
+            while (!v[i].empty() && j > v[i][0]) {
+                v[i].pop_front();
+            }
+            if (v[i].empty())
+                continue;
+            while (!t.empty() && t.top() <= ('a' + i)) {
+                p += t.top();
+                t.pop();
+            }
+            while (!v[i].empty()) {
+                if (j > v[i][0]) {
+                    v[i].pop_front();
+                    continue;
+                }
+                while (j <= v[i][0]) {
                     t.push(s[j]);
                     j++;
                 }
-                while(!t.empty() && t.top() == ('a'+i)){p+=t.top();t.pop();}
+                while (!t.empty() && t.top() == ('a' + i)) {
+                    p += t.top();
+                    t.pop();
+                }
             }
         }
-        while(!t.empty()){
-            p+=t.top();
+        while (!t.empty()) {
+            p += t.top();
             t.pop();
         }
         return p;
