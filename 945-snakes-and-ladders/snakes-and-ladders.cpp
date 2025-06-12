3116f453c9a5d23a786f77e284f84e1be9a0190f
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-12 21:28:50 +0530

    Time: 10 ms (13.14%) | Memory: 20.1 MB (7.08%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& b) {
-        int n = b.size(), m = b[0].size(); // declaring sizes..
-        vector<vector<int>> v1((n * m) + 1,vector<int>(2));   // a vector storing positions(i,j) equivalent to count
+        int n = b.size(); // declaring sizes..
+        vector<vector<int>> v1((n * n) + 1,vector<int>(2));   // a vector storing positions(i,j) equivalent to count
         int c = 1;     // starting with 1
         bool f = true; // helps in moving forward and backward accordint to Boustrophedon style
         for (int i = n - 1; i >= 0; i--) {
-            int j = f ? 0 : m - 1; // f decide we gonna move backward or forward... 
+            int j = f ? 0 : n - 1; // f decide we gonna move backward or forward... 
             while (true) { 
                 v1[c][0] = i;
                 v1[c][1] = j;
                 c++;
                 j += f ? 1 : -1;
-                if (f && j >= m)
+                if (f && j >= n)
                     break;
                 else if (!f && j < 0)
                     break;
@@ -22,6 +22,7 @@ public:
         queue<pair<int,int>>q;
         q.push({1,0});// first position along with it's minimum step(position,steps)
         vector<bool>visited(n*n + 1,false);
+        visited[1] = true;
         while(!q.empty()){
             int pos = q.front().first , moves = q.front().second;
             q.pop();
