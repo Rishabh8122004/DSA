e89383599203b8a2a9474aefddb0322ddf191e5e
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-12 21:30:15 +0530

    Time: 2 ms (55.19%) | Memory: 17.5 MB (27.07%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& b) {
         int n = b.size(); // declaring sizes..
-        vector<vector<int>> v1((n * n) + 1,vector<int>(2));   // a vector storing positions(i,j) equivalent to count
+        vector<pair<int,int>> v1((n * n) + 1);   // a vector storing positions(i,j) equivalent to count
         int c = 1;     // starting with 1
         bool f = true; // helps in moving forward and backward accordint to Boustrophedon style
         for (int i = n - 1; i >= 0; i--) {
             int j = f ? 0 : n - 1; // f decide we gonna move backward or forward... 
             while (true) { 
-                v1[c][0] = i;
-                v1[c][1] = j;
+                v1[c].first = i;
+                v1[c].second = j;
                 c++;
                 j += f ? 1 : -1;
                 if (f && j >= n)
@@ -30,7 +30,7 @@ public:
             for(int dice = 1;dice<=6;dice++){
                 if(pos+dice > n*n)break;
                 int next_pos = pos+dice;
-                int i = v1[next_pos][0],j = v1[next_pos][1];
+                int i = v1[next_pos].first,j = v1[next_pos].second;
                 if(b[i][j] != -1){
                     next_pos = b[i][j];
                 }
