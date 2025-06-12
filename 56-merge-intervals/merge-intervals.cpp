07bb7fed8359102bf9633d3ea5f6ffa2ac3e41e7
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-12 12:15:43 +0530

    Time: 5 ms (56.29%) | Memory: 24.3 MB (24.71%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
+    void check(vector<vector<int>>& ans){
+        int n = ans.size();
+        if(ans.size()>1){
+            if(ans[n-1][0]<=ans[n-2][0] || ans[n-1][1] <= ans[n-2][1]){
+                swap(ans[n-2],ans[n-1]);
+                ans.pop_back();
+            }
+        }
+        return;
+    }
     vector<vector<int>> merge(vector<vector<int>>& iv) {
         vector<vector<int>> ans;
         int n = iv.size();
+        if(n == 1)return iv;
         sort(iv.begin(),iv.end());
-        bool f = false;
-        while(!f){
-            f = true;
             for(int i = 0;i<n;i++){
+                bool f = true;
+                if(i == 1 && ans.empty()){ans.push_back(iv[0]);}
                 if(i<n-1 && iv[i][1] >= iv[i+1][0]){
                     iv[i][1] = max(iv[i][1],iv[i+1][1]);
                     ans.push_back({iv[i][0],iv[i][1]});
+                    iv[i+1] = iv[i];
                     f = false;
-                    i++;
-                    continue;
                 }
-                ans.push_back({iv[i][0],iv[i][1]});
+                else if(i>0 && iv[i][0]<=iv[i-1][1]){
+                    iv[i][0] = min(iv[i][0],iv[i-1][0]);
+                    ans.push_back(iv[i]);
+                    f = false;
+                }
+                if(f && !ans.empty() && ans[ans.size()-1] != iv[i]){
+                ans.push_back({iv[i]});
+                }
+                check(ans);
             }
-            iv = ans;
-            n = iv.size();
-            ans = {};
-        }
-        return iv;
+        return ans;
     }
 };
