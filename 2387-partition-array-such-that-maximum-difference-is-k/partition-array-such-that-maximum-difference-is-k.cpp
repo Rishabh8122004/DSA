cb9c06c611fdd889b1dc4686880cfc07c7085c64
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-19 08:33:28 +0530

    Time: 5 ms (97.53%) | Memory: 91 MB (5.15%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -4,17 +4,17 @@ public:
         int n = nums.size();
         int m = -1;
         for(int i = 0;i<n;i++)m = max(m,nums[i]);
-        vector<int>ioe(m+1,-1);//index of element
-        for(int i = 0;i<n;i++)ioe[nums[i]] = i;
+        vector<int>ioe(m+1,-1);//index of element of the size = max element in nums
+        for(int i = 0;i<n;i++)ioe[nums[i]] = i; // last index a num appeared , doesn't matter coz all the numbers(index) are already in sorted manner
         int ans = 0;
         for (int i = 0; i < m+1; i++) {
             if(ioe[i] == -1)continue;
             int limit = min(i+k,m);
-            while(ioe[limit] == -1){
-                limit--;
-            }
-            ans++;
-            i = limit;
+            // while(ioe[limit] == -1){ // not so necessary/// 
+            //     limit--;
+            // }
+            ans++; // though we reached our limit means we can't take any more in this range
+            i = limit; // gotta take 'i' to the next range
         }
         return ans;
     }
