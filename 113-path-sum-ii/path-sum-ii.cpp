c1639777c65bccefbcb295086f3f47f7d778356f
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-21 12:11:45 +0530

    Time: 16 ms (18.63%) | Memory: 40.8 MB (5.32%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void solve(TreeNode* root,vector<int>v,vector<vector<int>>&ans,int sum,int &ts){
+        if(!root){return;}
+        v.push_back(root->val);
+        if(!root->left && !root->right && sum+root->val == ts){ans.push_back(v);}
+        solve(root->left,v,ans,sum+root->val,ts);
+        solve(root->right,v,ans,sum+root->val,ts);
+        return;
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int ts) {
+        vector<vector<int>>ans;
+        vector<int>v;
+        solve(root,v,ans,0,ts);
+        return ans;
+    }
+};
