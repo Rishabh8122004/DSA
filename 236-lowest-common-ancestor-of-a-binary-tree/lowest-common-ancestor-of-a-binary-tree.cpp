1f047310d5e2160a243b456630efc2e0066b6c04
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-23 17:53:48 +0530

    Time: 11 ms (68.05%) | Memory: 18.9 MB (6.21%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
-    bool exist(TreeNode* root, TreeNode* & p){
-        if(!root)return false;
-        if(root == p)return true;
-        return exist(root->left,p)||exist(root->right,p);
+    void solve(TreeNode* root, vector<TreeNode*>& vec, TreeNode* &p1, bool &flag) {
+        if (flag || !root) return;
+
+        vec.push_back(root);
+        if (root == p1) {
+            flag = true;
+            return;
+        }
+
+        solve(root->left, vec, p1, flag);
+        solve(root->right, vec, p1, flag);
+
+        if (!flag) vec.pop_back();
     }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(exist(root->left,p) && exist(root->left,q))return lowestCommonAncestor(root->left,p,q);
-        else if(exist(root->right,p) && exist(root->right,q))return lowestCommonAncestor(root->right,p,q);
-        else return root;
+        vector<TreeNode*> pathP, pathQ;
+        bool flag = false;
+
+        solve(root, pathP, p, flag);
+        flag = false;
+        solve(root, pathQ, q, flag);
+
+        TreeNode* ans = nullptr;
+        int i = 0;
+        while (i < pathP.size() && i < pathQ.size()) {
+            if (pathP[i] == pathQ[i]) {
+                ans = pathP[i];
+            } else break;
+            i++;
+        }
+        return ans;
     }
 };
