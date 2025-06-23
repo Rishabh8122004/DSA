4e918ead1f7ca0229374401f13d60b0936f96e02
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-06-23 17:46:40 +0530

    Time: 26 ms (27.70%) | Memory: 23.6 MB (33.01%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    void findpath(TreeNode* root,TreeNode* &p, vector<TreeNode*>&v){
-        if(!root)return;
+    void findpath(TreeNode* root, TreeNode*& p, vector<TreeNode*>& v) {
+        if (!root)
+            return;
         v.push_back(root);
-        if(root->val == p->val)return;
-        if(p->val<root->val)findpath(root->left,p,v);
-        else findpath(root->right,p,v);
+        if (root->val == p->val)
+            return;
+        if (p->val < root->val)
+            findpath(root->left, p, v);
+        else
+            findpath(root->right, p, v);
         return;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>v,w;
-        findpath(root,p,v);
-        findpath(root,q,w);
+        vector<TreeNode*> v, w;
+        findpath(root, p, v);
+        findpath(root, q, w);
         int i = 0;
-        TreeNode*ans;
-        while(i<v.size()&&i<w.size()&& v[i] == w[i]){
+        TreeNode* ans;
+        while (i < v.size() && i < w.size() && v[i] == w[i]) {
             ans = v[i];
             i++;
         }
