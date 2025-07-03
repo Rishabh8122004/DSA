e3c352d2b9d1c6ac0b072a8b2f35ad6ba0e1ab1b
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-03 15:53:57 +0530

    Time: 27 ms (23.32%) | Memory: 44.2 MB (6.54%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -3,127 +3,145 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         vector<int>v;
         int n = arr.size();
-        if(x<=arr[0])
-        {
-            for(int i = 0;i<k;i++)
-            {
-                v.push_back(arr[0+i]);
+        // my solution : below this there is also a solution using maxheap ;
+        // if(x<=arr[0])
+        // {
+        //     for(int i = 0;i<k;i++)
+        //     {
+        //         v.push_back(arr[0+i]);
+        //     }
+        // }
+        // else if(x>=arr[n-1])
+        // {
+        //     for(int i = 0;i<k;i++)
+        //     {
+        //         v.push_back(arr[n-1-i]);
+        //     }
+        //     reverse(v.begin(),v.end());
+        // }
+        // else{
+        //     int lo = 0,hi = n-1;
+        //     bool check = false;
+        //     int lb,ub,idx;
+        //     while(lo <= hi)
+        //     {
+        //         int mid = lo+(hi-lo)/2;
+        //         if(arr[mid] == x){check = true;idx = mid; lb=mid-1;ub =mid+1;break;}
+        //         else if(arr[mid]<x){lo = mid+1;}
+        //         else{hi = mid-1;}
+        //     }
+        //     if(check == true)
+        //     {
+        //         v.push_back(arr[idx]);
+        //          for(int i = 0;i<k-1;i++)
+        //         {
+        //             if(abs(arr[lb]-x)==abs(arr[ub]-x))
+        //             {
+        //             v.push_back(arr[lb]);
+        //             lb--;
+        //             if(lb<0)
+        //             {
+        //                while(v.size()<k)
+        //                {
+        //                 v.push_back(arr[ub]);ub++;
+        //                }
+        //                break;
+        //             }
+        //             }
+        //             else if(abs(arr[lb]-x)>abs(arr[ub]-x)){
+        //             if(v.size()==k){break;}
+        //             v.push_back(arr[ub]);
+        //             ub++;
+        //             if(ub>n-1)
+        //             {
+        //                 while(v.size()<k)
+        //                 {
+        //                     v.push_back(arr[lb]);lb--;
+        //                 }
+        //                 break;
+        //             }
+        //             }
+        //             else if(abs(arr[lb]-x)<abs(arr[ub]-x))
+        //             {
+        //             v.push_back(arr[lb]);
+        //             lb--;
+        //             if(lb<0)
+        //             {
+        //                while(v.size()<k)
+        //                {
+        //                 v.push_back(arr[ub]);ub++;
+        //                }
+        //                break;
+        //             }
+        //             }
+        //         }
+        //         sort(v.begin(),v.end());
+        //     }
+        //     else{
+        //         ub = lo;lb = hi;
+        //        for(int i = 0;i<k;i++)
+        //         {
+        //             if(abs(arr[lb]-x)==abs(arr[ub]-x))
+        //             {
+        //             v.push_back(arr[lb]);
+        //             lb--;
+        //             if(lb<0)
+        //             {
+        //                while(v.size()<k)
+        //                {
+        //                 v.push_back(arr[ub]);ub++;
+        //                }
+        //                 break;
+        //             }
+        //             }
+        //             else if(abs(arr[lb]-x)>abs(arr[ub]-x))
+        //             {
+        //             if(v.size()==k){break;}
+        //             v.push_back(arr[ub]);
+        //             ub++;
+        //             if(ub>n-1)
+        //             {
+        //                 while(v.size()<k)
+        //                 {
+        //                     v.push_back(arr[lb]);lb--;
+        //                 }
+        //                 break;
+        //             }
+        //             }
+        //             else if(abs(arr[lb]-x)<abs(arr[ub]-x))
+        //             {
+        //             v.push_back(arr[lb]);
+        //             lb--;
+        //             if(lb<0)
+        //             {
+        //                while(v.size()<k)
+        //                {
+        //                 v.push_back(arr[ub]);ub++;
+        //                }
+        //                 break;
+        //             }
+        //             }
+        //         }
+        //         sort(v.begin(),v.end());
+        //     }
+        // }
+        // max heap solution : 
+        vector<pair<int,int>>dis;
+        for(int i = 0;i<n;i++){
+            dis.push_back({abs(x-arr[i]),arr[i]});
+        }
+        priority_queue<pair<int,int>>pq;
+        for(int i = 0;i<n;i++){
+            pq.push(dis[i]);
+            if(pq.size()>k){
+                pq.pop();
             }
         }
-        else if(x>=arr[n-1])
-        {
-            for(int i = 0;i<k;i++)
-            {
-                v.push_back(arr[n-1-i]);
-            }
-            reverse(v.begin(),v.end());
+        while(pq.size()){
+            v.push_back(pq.top().second);
+            pq.pop();
         }
-        else{
-            int lo = 0,hi = n-1;
-            bool check = false;
-            int lb,ub,idx;
-            while(lo <= hi)
-            {
-                int mid = lo+(hi-lo)/2;
-                if(arr[mid] == x){check = true;idx = mid; lb=mid-1;ub =mid+1;break;}
-                else if(arr[mid]<x){lo = mid+1;}
-                else{hi = mid-1;}
-            }
-            if(check == true)
-            {
-                v.push_back(arr[idx]);
-                 for(int i = 0;i<k-1;i++)
-                {
-                    if(abs(arr[lb]-x)==abs(arr[ub]-x))
-                    {
-                    v.push_back(arr[lb]);
-                    lb--;
-                    if(lb<0)
-                    {
-                       while(v.size()<k)
-                       {
-                        v.push_back(arr[ub]);ub++;
-                       }
-                       break;
-                    }
-                    }
-                    else if(abs(arr[lb]-x)>abs(arr[ub]-x)){
-                    if(v.size()==k){break;}
-                    v.push_back(arr[ub]);
-                    ub++;
-                    if(ub>n-1)
-                    {
-                        while(v.size()<k)
-                        {
-                            v.push_back(arr[lb]);lb--;
-                        }
-                        break;
-                    }
-                    }
-                    else if(abs(arr[lb]-x)<abs(arr[ub]-x))
-                    {
-                    v.push_back(arr[lb]);
-                    lb--;
-                    if(lb<0)
-                    {
-                       while(v.size()<k)
-                       {
-                        v.push_back(arr[ub]);ub++;
-                       }
-                       break;
-                    }
-                    }
-                }
-                sort(v.begin(),v.end());
-            }
-            else{
-                ub = lo;lb = hi;
-               for(int i = 0;i<k;i++)
-                {
-                    if(abs(arr[lb]-x)==abs(arr[ub]-x))
-                    {
-                    v.push_back(arr[lb]);
-                    lb--;
-                    if(lb<0)
-                    {
-                       while(v.size()<k)
-                       {
-                        v.push_back(arr[ub]);ub++;
-                       }
-                        break;
-                    }
-                    }
-                    else if(abs(arr[lb]-x)>abs(arr[ub]-x))
-                    {
-                    if(v.size()==k){break;}
-                    v.push_back(arr[ub]);
-                    ub++;
-                    if(ub>n-1)
-                    {
-                        while(v.size()<k)
-                        {
-                            v.push_back(arr[lb]);lb--;
-                        }
-                        break;
-                    }
-                    }
-                    else if(abs(arr[lb]-x)<abs(arr[ub]-x))
-                    {
-                    v.push_back(arr[lb]);
-                    lb--;
-                    if(lb<0)
-                    {
-                       while(v.size()<k)
-                       {
-                        v.push_back(arr[ub]);ub++;
-                       }
-                        break;
-                    }
-                    }
-                }
-                sort(v.begin(),v.end());
-            }
-        }
+        sort(v.begin(),v.end());
         return v;
     }
 };
