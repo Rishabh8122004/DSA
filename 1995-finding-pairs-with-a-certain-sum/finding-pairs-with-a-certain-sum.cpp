bc32d90ba073e6124054cde2e8f741b6b1695eaa
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-06 10:15:31 +0530

    Time: 675 ms (11.69%) | Memory: 194.2 MB (8.98%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -4,16 +4,15 @@ public:
     map<int, int> mp; // value from q -> frequency
 
     FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
-        this->p = nums1;
-        this->q = nums2;
+        p = nums1;
+        q = nums2;
         for (int num : q) {
             mp[num]++;
         }
     }
 
     void add(int index, int val) {
-        int old_val = q[index];
-        mp[old_val]--;
+        mp[q[index]]--;
         q[index] += val;
         mp[q[index]]++;
     }
@@ -21,8 +20,7 @@ public:
     int count(int tot) {
         int ans = 0;
         for (int num : p) {
-            int complement = tot - num;
-            ans += mp[complement];
+            ans += mp[tot - num];
         }
         return ans;
     }
