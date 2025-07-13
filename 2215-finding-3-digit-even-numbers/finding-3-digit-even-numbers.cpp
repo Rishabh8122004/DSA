b777ebad2ea27ac4b20ee2189051f5c4df208e59
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-13 18:46:59 +0530

    Time: 57 ms (37.55%) | Memory: 31.9 MB (6.32%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -14,7 +14,7 @@ public:
         unordered_map<int, int> mp;
         for (int e : d)
             mp[e]++;
-        for (int i = 100; i < 1000; i++) { // possible range of outcomes
+        for (int i = 100; i < 1000; i+=2) { // possible range of outcomes
             if ( i%2 == 0 && isvalid(i, mp)) {
                 ans.push_back(i);
             }
