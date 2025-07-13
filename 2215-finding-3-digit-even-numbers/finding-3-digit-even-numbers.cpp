e6c5f76c6f3086f98f8663125b4554d448f500f1
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-13 18:45:18 +0530

    Time: 61 ms (36.44%) | Memory: 32 MB (5.80%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isvalid(int x, unordered_map<int, int> mp) {
+        while (x) {
+            if (mp[x % 10] == 0)
+                return false;
+            mp[x % 10]--;
+            x /= 10;
+        }
+        return true;
+    }
+    vector<int> findEvenNumbers(vector<int>& d) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for (int e : d)
+            mp[e]++;
+        for (int i = 100; i < 1000; i++) { // possible range of outcomes
+            if ( i%2 == 0 && isvalid(i, mp)) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
