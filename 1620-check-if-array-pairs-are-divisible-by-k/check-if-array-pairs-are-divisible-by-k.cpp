548e0aa7f29912292c765739fa15f411547d01a0
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-14 16:59:41 +0530

    Time: 36 ms (74.37%) | Memory: 83.4 MB (60.57%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        int n = arr.size();
+        if (k == 1)
+            return true;
+        unordered_map<int, int>
+            mp; // (arr[i]%k) and number of elements having same result
+        for (int i = 0; i < arr.size(); i++) {
+            mp[((arr[i] % k)+k)%k]++;
+        }
+        if (mp[0] % 2 != 0)
+            return false; // odd number of elements with divisiblity by k
+        for (auto p : mp) {
+            if (p.first == 0)
+                continue;
+            if (mp[k - p.first] != p.second) return false;
+        }
+        return true;
+    }
+};
