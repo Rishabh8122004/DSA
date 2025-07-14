4db2261377bbbac4abba5eebbc3c216f8e219b4d
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-14 23:15:50 +0530

    Time: 3 ms (66.64%) | Memory: 8.3 MB (11.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -15,8 +15,8 @@ public:
     // tabulation or iterative method;
     int fib(int n) {
         if(n == 0)return 0;
-        vector<int>dp(n+1,-1);
-        dp[1] = 1;dp[0] = 0;
+        vector<int>dp(n+1);
+        dp[1] = 1;
         for(int i = 2;i<=n;i++){
             dp[i] = dp[i-1]+dp[i-2];
         }
