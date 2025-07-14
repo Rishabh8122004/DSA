855594dadadf7c898337f585238a46de1592f5a1
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-14 16:59:51 +0530

    Time: 35 ms (76.99%) | Memory: 83.3 MB (60.57%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -12,8 +12,7 @@ public:
         if (mp[0] % 2 != 0)
             return false; // odd number of elements with divisiblity by k
         for (auto p : mp) {
-            if (p.first == 0)
-                continue;
+            if (p.first == 0) continue;
             if (mp[k - p.first] != p.second) return false;
         }
         return true;
