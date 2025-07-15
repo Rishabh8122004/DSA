109d530c8120a1cef3fa483ad605470cefa62a3b
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-15 10:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.43%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    if(n==1) return 1;
+    if(n==2) return 2;
+        int a = 1;
+        int b = 2;
+        int sum;
+        for(int i = 0;i<n-2;i++)
+        {
+            sum = a+b;
+            a=b;
+            b = sum;
+        }
+        return sum;
+    }
+};
