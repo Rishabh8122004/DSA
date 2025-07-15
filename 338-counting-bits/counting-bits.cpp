309ec996a2bf7f4137de3eeee174860fb860aa33
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-15 11:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (80.89%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -3,11 +3,10 @@ public:
     vector<int> countBits(int n) {
         if(n == 0)return {0};
         if(n == 1)return {0,1};
-        vector<int>dp(n+1,0); // every number has at least 1 set bit except 0
-        dp[1] = 1; // 0 has no set bit
+        vector<int>dp(n+1,1); // every number has at least 1 set bit except 0
+        dp[0] = 0; // 0 has no set bit
         for(int i = 2;i<=n;i++){
-        dp[i] += dp[i/2];
-        if(i%2 == 1)dp[i]++;
+        dp[i] += dp[i&(i-1)]; 
         }
         return dp;
     }
