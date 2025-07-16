7844aeb4126be6ccecf7a37d3617820415c59c8c
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-16 10:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.33%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)return nums[0];
+        for(int i = 0;i<n;i++){
+            if(i-3 >= 0){
+                nums[i] += max(nums[i-2],nums[i-3]);
+            }
+            else if(i-2 >= 0){
+                nums[i] += nums[i-2];
+            }
+        }
+        return max(nums[n-1],nums[n-2]);
+    }
+};
