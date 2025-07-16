97740156432b155f567a3d198849f865d473770e
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-16 20:18:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (56.15%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    int getsubarray(int x){
-        if(x<3)return 0;
-        int ans = 0;
-        int y = 3;
-        while(y<=x){
-            ans += (x-y+1);
-            y++;
-        }
-        return ans;
-    }
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
         if(n < 3)return 0;
@@ -18,12 +8,13 @@ public:
         int d = nums[n-1]-nums[n-2];
         for(int i = n-2;i>0;i--){
             if(nums[i]-nums[i-1] != d){
-                ans += getsubarray(len);
+                ans += ((len-2)*(len-1))/2;
                 len = 2;
                 d = nums[i]-nums[i-1]; 
             }
             else len++;
         }
-        return ans+getsubarray(len);
+        ans += ((len-2)*(len-1))/2;
+        return ans;
     }
 };
