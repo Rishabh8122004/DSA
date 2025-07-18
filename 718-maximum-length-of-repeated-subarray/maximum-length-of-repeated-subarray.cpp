44d094f7b13c36db795996d3acf3e89a324f1d4b
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-19 00:43:03 +0530

    Time: 139 ms (18.14%) | Memory: 89.4 MB (31.92%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -24,18 +24,12 @@ public:
         }*/
         int n = n1.size(),m = n2.size();
         vector<vector<int>>dp(m+1,vector<int>(n+1));
-        for(int i = 1;i<=n;i++){
-            dp[0][i] = n1[i-1];
-        }
-        for(int j = 1;j<=m;j++){
-            dp[j][0] = n2[j-1];
-        }
         for(int i = 1;i<=m;i++){
             for(int j = 1;j<=n;j++){
                 if(i == 1 || j == 1){
-                    if(dp[0][j] == dp[i][0]){dp[i][j] = 1;}
+                    if(n1[j-1] == n2[i-1]){dp[i][j] = 1;}
                 }
-                else{dp[i][j] = ((dp[0][j] == dp[i][0])?(1+dp[i-1][j-1]):0);}
+                else{dp[i][j] = ((n1[j-1] == n2[i-1])?(1+dp[i-1][j-1]):0);}
                 ans = max(ans,dp[i][j]);
             }
         }
