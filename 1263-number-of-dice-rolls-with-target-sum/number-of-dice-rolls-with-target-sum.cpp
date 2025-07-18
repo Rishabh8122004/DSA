04a36814b74eda41fafae0735462f6f02f1706b4
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-18 20:19:15 +0530

    Time: 31 ms (10.02%) | Memory: 13.4 MB (16.37%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        int a = 1e9+7;
+        vector<vector<long long >>dp(n+1,vector<long long>(target+1,0));
+        dp[0][0] = 1;
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=target;j++){
+                int K = 1;
+                while(K<=k){
+                    if(j-K>=0)dp[i][j] += dp[i-1][j-K];
+                    dp[i][j]%=a;
+                    K++;
+                }
+                dp[i][j]%=a;
+            }
+        }
+        return dp[n][target];
+    }
+};
