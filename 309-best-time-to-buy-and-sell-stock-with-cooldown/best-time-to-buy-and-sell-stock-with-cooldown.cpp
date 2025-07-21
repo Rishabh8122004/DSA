f09ec289703d6a573e63390a91361c353090a293
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-07-21 16:31:06 +0530

    Time: 15 ms (5.90%) | Memory: 20.8 MB (5.43%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-    int solve(vector<int>&prices,int day,int &n,bool buy,vector<vector<int>>&dp){
-        if(day>=n)return 0;
-        if(dp[day][buy] != INT_MIN)return dp[day][buy];
-        int profit = INT_MIN;
-        if(buy){
-            int taken = solve(prices,day+1,n,false,dp)-prices[day];
-            int not_taken = solve(prices,day+1,n,true,dp);
-            profit =  max(profit,max(taken,not_taken));
-        }
-        else{
-            int sold = solve(prices,day+2,n,true,dp)+prices[day];
-            int not_sold = solve(prices,day+1,n,false,dp);
-            profit = max(profit,max(sold,not_sold));
-        }
-        return dp[day][buy] = profit;
-    }
+    // int solve(vector<int>&prices,int day,int &n,bool buy,vector<vector<int>>&dp){
+    //     if(day>=n)return 0;
+    //     if(dp[day][buy] != INT_MIN)return dp[day][buy];
+    //     int profit = INT_MIN;
+    //     if(buy){
+    //         int taken = solve(prices,day+1,n,false,dp)-prices[day];
+    //         int not_taken = solve(prices,day+1,n,true,dp);
+    //         profit =  max(profit,max(taken,not_taken));
+    //     }
+    //     else{
+    //         int sold = solve(prices,day+2,n,true,dp)+prices[day];
+    //         int not_sold = solve(prices,day+1,n,false,dp);
+    //         profit = max(profit,max(sold,not_sold));
+    //     }
+    //     return dp[day][buy] = profit;
+    // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
+        if(n == 1)return 0;
         bool buy = true;
-        vector<vector<int>>dp(5000,vector<int>(2,INT_MIN));
-        return solve(prices,0,n,buy,dp);
+        //vector<vector<int>>dp(5000,vector<int>(2,INT_MIN));
+        //return solve(prices,0,n,buy,dp);
+        //TABULATION METHOD
+        vector<int>dp(5000,0);
+        dp[1] = max(prices[1]-prices[0],0);
+        for(int i = 2;i<n;i++){
+            dp[i] = dp[i-1];
+            for(int j = 0;j<i;j++){
+                dp[i] = max(dp[i],(prices[i]-prices[j]+(j>1?dp[j-2]:0)));
+            }
+        }
+        return dp[n-1];
     }
 };
