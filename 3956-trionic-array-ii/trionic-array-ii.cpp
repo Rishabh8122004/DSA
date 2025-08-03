129bf64546ed3a34be555efe2e9408614a26f63d
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-08-03 12:27:48 +0530

    Time: 97 ms (10.00%) | Memory: 132.5 MB (100.00%) - LeetSync

---
+++ 3956-trionic-array-ii/trionic-array-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    long long maxSumTrionic(vector<int>& v) {
+        int n = v.size();
+        int idx = 0;
+        long long ans = LLONG_MIN;
+        while (idx < n - 3) {
+            // finding the trionic array starting from idx
+            // first increasing part
+            int l = idx;
+            int p = l, q;
+            long long first_increasing = v[p];
+            while (p < n - 1 && v[p] < v[p + 1]) {
+                p++;
+                first_increasing += v[p];
+                if(v[p]>first_increasing && p < n - 1 && v[p] < v[p + 1]) first_increasing = v[p];
+            }
+            if (p >= n - 2 || p == l) {
+                idx++;
+                continue;
+            } // not a valid trionic
+            // decreasing part
+            q = p + 1;
+            if (v[q] >= v[p]) {
+                idx++;
+                continue;
+            } // not a valid one..
+            long long decreasing = v[q];
+            while (q < n - 1 && v[q] > v[q + 1]) {
+                q++;
+                decreasing += v[q];// got no choice in this one.. 
+            }
+            if (q == n - 1) {
+                idx++;
+                continue;
+            }
+            // now comes the second increasing
+            long long second_increasing = 0;
+            int r = q,j = r;
+            while (r < n - 1 && v[r] < v[r + 1]){
+                r++;
+                second_increasing += v[r];
+                if(v[r]<0 && r < n - 1 && v[r] < v[r + 1]){j=r;}
+                ans = max(ans, first_increasing+decreasing+second_increasing); 
+            }
+            if (r == q) {
+                idx++;
+                continue;
+            } 
+            idx = j; // ensures progress even if r == idx
+        }
+        return ans;
+    }
+};
