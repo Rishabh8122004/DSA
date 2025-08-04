0583b6b40255752619e14a157cd173831e2a3315
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2025-08-04 11:26:37 +0530

    Time: 64 ms (5.58%) | Memory: 32.4 MB (5.48%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -4,7 +4,7 @@ public:
         if(k == 0 || i>=p.size())return 0;
         if(dp[i][k][buy] != INT_MIN)return dp[i][k][buy];
         int ans = 0;
-        if(buy && k>0){
+        if(buy){
             ans += max(solve(p,i+1,k,false,dp)-p[i],solve(p,i+1,k,true,dp));
         }
         if(!buy){
