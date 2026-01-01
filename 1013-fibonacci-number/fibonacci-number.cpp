bdb5ccb39d76ae60e4fc74e1c25764d8f3c7a150
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-01-01 16:03:06 +0530

    Time: 2 ms (76.78%) | Memory: 7.8 MB (36.12%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
- // recursion + memoisation method
-    // int solve(int n,vector<int>&dp){
-    //     if(dp[n] != -1)return dp[n];
-    //     return dp[n] = solve(n-1,dp)+solve(n-2,dp);;
-    // }
-    // int fib(int n) {
-    //     if(n == 0)return 0;
-    //     vector<int>dp(n+1,-1);
-    //     dp[1] = 1;dp[0] = 0;
-    //     solve(n,dp);
-    //     return solve(n,dp);;
-    // }
-    // tabulation or iterative method;
+    int solve(vector<int>&dp,int n){
+        if(n == 0)return 0;
+        if(n == 1)return 1;
+        if(dp[n] != -1)return dp[n];
+        return dp[n] = solve(dp,n-1)+solve(dp,n-2);
+    }
     int fib(int n) {
-        if(n <= 1)return n;
-        vector<int>dp(n+1,-1);
-        dp[1] = 1;dp[0] = 0;
-        for(int i = 2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        vector<int>dp(31,-1);
+        return solve(dp,n);
     }
 };
