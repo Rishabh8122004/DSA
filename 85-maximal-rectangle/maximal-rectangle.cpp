9e2854f609cd808b9e146afc1a07a40ed6b7a3d0
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-01-11 11:08:56 +0530

    Time: 12 ms (25.18%) | Memory: 20.6 MB (28.70%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -4,23 +4,25 @@ public:
         int n = v.size();
         vector<int>leftest_short(n,-1),rightest_short(n,-1);
         stack<int>st;
-        for(int i = 1;i<n;i++){
-            st.push(i-1);
+        for(int i = 0;i<n;i++){
             while(!st.empty() && v[st.top()]>=v[i])st.pop();
-            if(st.size())leftest_short[i] = st.top();
+            if(st.size())leftest_short[i] = i-st.top();
+            else leftest_short[i] = i+1;
+            st.push(i);
         }
         while(!st.empty())st.pop();
-        for(int i = n-2;i>-1;i--){
-            st.push(i+1);
+        for(int i = n-1;i>-1;i--){
             while(!st.empty() && v[st.top()]>=v[i])st.pop();
-            if(st.size())rightest_short[i] = st.top();
+            if(st.size())rightest_short[i] = st.top()-i;
+            else rightest_short[i] = n-i;
+            st.push(i);
         }
         int ans = 0;
         int x;
         for(int i = 0;i<n;i++){
             if(v[i] == 0)continue;
-            x   = (leftest_short[i] == -1?v[i]*(i+1):v[i]*(i-leftest_short[i]));
-            x  += (rightest_short[i] == -1?v[i]*(n-i):v[i]*(rightest_short[i]-i));
+            x   = v[i]*(leftest_short[i]);
+            x  += v[i]*(rightest_short[i]);
             x  -= v[i];
             ans = max(ans,x);
         }
