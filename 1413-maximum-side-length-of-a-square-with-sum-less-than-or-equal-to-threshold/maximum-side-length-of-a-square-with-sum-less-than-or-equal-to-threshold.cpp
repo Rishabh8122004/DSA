2d2c42fe55e5a9f274fc3791b27c772e703cd79b
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-01-19 10:08:37 +0530

    Time: 122 ms (18.64%) | Memory: 32.8 MB (36.82%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int get_minsum(int &k,vector<vector<int>>&r,int &threshold){
+    int get_minsum(int k,vector<vector<int>>&r,int &threshold){
         int sum = INT_MAX,a,temp = 0;
         for(int i = 0;i+k-1<r.size();i++){
             for(int j = 0;j+k-1<r[0].size();j++){
@@ -24,11 +24,13 @@ public:
                 r[i][j] += r[i][j-1];
             }
         }
-        int k = 1;
-        while(k <= min(m,n)){
-            if(get_minsum(k,r,threshold) > threshold)return k-1;
-            k++;
+        int lo = 1,hi = min(m,n);
+        int ans = 0;
+        while(lo<=hi){
+            int mid = (lo+(hi-lo)/2);
+            if(get_minsum(mid,r,threshold)<=threshold){ans = max(ans,mid);lo = mid+1;}
+            else hi = mid-1;
         }
-        return k-1;
+        return ans;
     }
 };
