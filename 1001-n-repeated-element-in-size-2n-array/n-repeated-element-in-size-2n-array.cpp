ff4ca586eaf0e5552b2cf371ac75794667542ca1
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-01-29 17:01:22 +0530

    Time: 109 ms (5.30%) | Memory: 28.3 MB (84.77%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n = nums.size();
+        n/=2;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<n*2;i++){
+            int x = nums[i],j = i;
+            while(nums[i] == x){
+                i++;
+                if(i == n*2)break;
+            }
+            cout<<i-j<<" ";
+            if(i-j == n)return nums[j];
+            i--;
+        }
+        return -1;
+    }
+};
