a4b30fb6cf32363df294e93ed5aa005a0f01fca2
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-03-14 09:14:17 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (74.83%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -3,17 +3,17 @@ public:
     string getHappyString(int n, int k) {
         if(k>(3*(pow(2,n-1))))return ""; // n bit string -> 3*(2*2*2....(n-1 times)) 
         string ans = "";
-        int mg = 3*(pow(2,n-1));
+        int mg = 3*(pow(2,n-1));// maximum generation we can do using n bits
         for(int i = 0;i<n;i++){
-            if(i == 0){
+            if(i == 0){ // the very first bit can contain any of the '3' characters
                 if(k<=mg/3)ans+='a';
                 else if(k<=(2*mg/3))ans+='b';
                 else ans+='c';
-                mg/=3;
+                mg/=3;// becasue each character holds only combinations of 1/3rd of the mg
                 k %= mg;
                 if(k == 0) k = mg;
             }
-            else{
+            else{ // all the non-first bits can contain any of the '2' characters
                 if(k<=mg/2){
                     if(ans.back() == 'a')ans+='b';
                     else ans+='a';
@@ -22,7 +22,7 @@ public:
                     if(ans.back() == 'c')ans+='b';
                     else ans+='c';
                 }
-                mg /= 2;
+                mg /= 2; // becasue here, each character holds half of all the combinations 
                 k %= mg;
                 if(k == 0)k = mg;
             }
