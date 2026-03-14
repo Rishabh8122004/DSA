ac6d525b9136da916e288279d1531546af7e9739
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-03-14 09:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (89.21%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        if (k > (3 * (pow(2, n - 1))))
-            return ""; // n bit string -> 3*(2*2*2....(n-1 times))
+        if(k>(3*(pow(2,n-1))))return ""; // n bit string -> 3*(2*2*2....(n-1 times)) 
         string ans = "";
-        int mg = 3 * (pow(2, n - 1));
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                if (k <= mg / 3)
-                    ans += 'a';
-                else if (k <= (2 * mg / 3))
-                    ans += 'b';
-                else
-                    ans += 'c';
-                mg /= 3;
+        int mg = 3*(pow(2,n-1));
+        for(int i = 0;i<n;i++){
+            if(i == 0){
+                if(k<=mg/3)ans+='a';
+                else if(k<=(2*mg/3))ans+='b';
+                else ans+='c';
+                mg/=3;
                 k %= mg;
-                if (k == 0)
-                    k = mg;
-            } else {
-                if (k <= mg / 2) {
-                    if (ans.back() == 'a')
-                        ans += 'b';
-                    else
-                        ans += 'a';
-                } else {
-                    if (ans.back() == 'c')
-                        ans += 'b';
-                    else
-                        ans += 'c';
+                if(k == 0) k = mg;
+            }
+            else{
+                if(k<=mg/2){
+                    if(ans.back() == 'a')ans+='b';
+                    else ans+='a';
+                }
+                else{
+                    if(ans.back() == 'c')ans+='b';
+                    else ans+='c';
                 }
                 mg /= 2;
                 k %= mg;
-                if (k == 0)
-                    k = mg;
+                if(k == 0)k = mg;
             }
         }
         return ans;
