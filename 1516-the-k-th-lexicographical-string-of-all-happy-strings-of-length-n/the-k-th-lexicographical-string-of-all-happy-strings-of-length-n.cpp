c9d02a6b6ded2c1607b5af3374f9ab2002491a30
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-03-14 09:10:23 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (80.34%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
-    string ans = "";
-    void solve(int &n,int &k,string s){
-        if(s.size() == n){
-            k--;
-            if(k == 0)ans = s;
-            return;
-        }
-        if(s.empty() || s.back() != 'a')solve(n,k,s+'a');
-        if(s.empty() || s.back() != 'b')solve(n,k,s+'b');
-        if(s.empty() || s.back() != 'c')solve(n,k,s+'c');
-        return;
-    }
     string getHappyString(int n, int k) {
-        if(k>(3*pow(2,n-1)))return ""; // k is greater than number of strings we can generate using n spaces.
-        solve(n,k,"");
+        if (k > (3 * (pow(2, n - 1))))
+            return ""; // n bit string -> 3*(2*2*2....(n-1 times))
+        string ans = "";
+        int mg = 3 * (pow(2, n - 1));
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                if (k <= mg / 3)
+                    ans += 'a';
+                else if (k <= (2 * mg / 3))
+                    ans += 'b';
+                else
+                    ans += 'c';
+                mg /= 3;
+                k %= mg;
+                if (k == 0)
+                    k = mg;
+            } else {
+                if (k <= mg / 2) {
+                    if (ans.back() == 'a')
+                        ans += 'b';
+                    else
+                        ans += 'a';
+                } else {
+                    if (ans.back() == 'c')
+                        ans += 'b';
+                    else
+                        ans += 'c';
+                }
+                mg /= 2;
+                k %= mg;
+                if (k == 0)
+                    k = mg;
+            }
+        }
         return ans;
     }
 };
