9ce8140962646ea72dc68ed2990e7e6c36d1fd5b
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-03-22 09:23:06 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (87.37%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        vector<int>f(101,false);
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]%2 == 0)f[nums[i]]++;
+        }
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]%2 == 0 && f[nums[i]] == 1)return nums[i];
+        }
+        return -1;
+    }
+};
