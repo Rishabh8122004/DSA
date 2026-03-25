a2c7c26ebc72a374443ca1b6ecdfd44334ed513b
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-03-25 19:31:15 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (5.09%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -12,10 +12,12 @@ public:
         }
         vector<vector<int>> ans(m,vector<int>(n,0));
         for(int i = 0;i<m;i++){
+            if(!v[i])continue;
             for(int j = 0;j<n;j++){
-                if(!v[i] || !v1[j]) matrix[i][j] = 0;
+                if(v1[j])ans[i][j] = matrix[i][j];
             }
         }
+        matrix = ans;
         return ;
     }
 };
