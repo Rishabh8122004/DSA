bf164e8db99b22bd5351cd5da06cb0a58a2e1c69
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-03-29 15:12:56 +0530

    Time: 2 ms (95.33%) | Memory: 19.5 MB (92.00%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -5,16 +5,16 @@ public:
         vector<int>even(26,0),odd(26,0);
         for(int i = 0;i<n;i++){
             if(i%2 == 0){
-                even[s1[i]-'a']++;
-                even[s2[i]-'a']--;
+                even[s1[i]-97]++;
+                even[s2[i]-97]--;
             }
             else{
-                odd[s1[i]-'a']++;
-                odd[s2[i]-'a']--;
+                odd[s1[i]-97]++;
+                odd[s2[i]-97]--;
             }
         }
         for(int i = 0;i<26;i++){
-            if(even[i] != 0 || odd[i] != 0)return false;
+            if(even[i] || odd[i])return false;
         }
         return true;
     }
