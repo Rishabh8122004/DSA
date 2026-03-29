bedd0620fa5411576c26e562b9056a6f4eba7e1c
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-03-29 19:40:52 +0530

    Time: 255 ms (5.03%) | Memory: 8.9 MB (21.97%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -5,27 +5,28 @@ public:
     unordered_map<int,int>diag1;
     unordered_map<int,int>diag2;
     int ans = 0;
-    bool solve(int r,int c,int Q,int& n){
+    void solve(int r,int c,int Q,int& n){
         if(r == n){
-            if(Q == n){ans++;return true;}
-            else return false;
+            if(Q == n){ans++;}
+            return ;
         }
         if(c == n){
-            return solve(r+1,0,Q,n);
+            solve(r+1,0,Q,n);
+            return;
         }
         if(row[r] == 0 && col[c] == 0 && diag1[r-c] == 0 && diag2[r+c] == 0 && Q < n){
             row[r] = 1;
             col[c] = 1;
             diag1[r-c] = 1;
             diag2[r+c] = 1;
-            if(!solve(r,c+1,Q+1,n)){
-            }
+            solve(r,c+1,Q+1,n);
             diag1[r-c] = 0;
             diag2[r+c] = 0;
             row[r] = 0;
             col[c] = 0;
         }
-        return solve(r,c+1,Q,n);
+        solve(r,c+1,Q,n);
+        return;
     }
     int totalNQueens(int n) {
         if(n == 1)return 1;
