fa74c9244ab418727436837e435bcbf19566f4ca
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-05-06 07:22:02 +0530

    Time: 12 ms (57.25%) | Memory: 56.8 MB (69.53%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -3,16 +3,16 @@ public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& g) {
         int n = g.size(),m = g[0].size();
         vector<vector<char>>ans(m,vector<char>(n,'.'));
-        // moving stones to the right
+        // filling the rotated matrix while keeping the positions of stones just above the obstacle/next stone/floor
         for(int i = 0;i<n;i++){
             int k = m-1;
             for(int j = m-1;j>=0;j--){
                 if(g[i][j] == '*'){
-                   ans[j][n-i-1] = g[i][j];
-                   k = j-1;
+                   ans[j][n-i-1] = g[i][j]; // an obstacle position won't change
+                   k = j-1; // obstacle is a something which can hold stones above it.. k = j-1.
                 }
                 else if(g[i][j] == '#'){
-                    ans[k][n-i-1] = g[i][j];
+                    ans[k][n-i-1] = g[i][j]; // only move k to the left when being placed a stone.
                     k--;
                 }
             }
