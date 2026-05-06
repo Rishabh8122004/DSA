57ff99aef14182cee181746025cdde219f8ebf74
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-05-06 07:19:18 +0530

    Time: 8 ms (81.33%) | Memory: 56.7 MB (90.17%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& g) {
         int n = g.size(),m = g[0].size();
+        vector<vector<char>>ans(m,vector<char>(n,'.'));
         // moving stones to the right
         for(int i = 0;i<n;i++){
+            int k = m-1;
             for(int j = m-1;j>=0;j--){
-                if(g[i][j] == '#'){
-                    int k = j;
-                    while(k+1<m && g[i][k+1] == '.'){
-                        k++;
-                    }
-                    swap(g[i][j],g[i][k]);
+                if(g[i][j] == '*'){
+                   ans[j][n-i-1] = g[i][j];
+                   k = j-1;
                 }
-            }
-        }
-        vector<vector<char>>ans(m,vector<char>(n,'.'));
-        for(int j = 0;j<m;j++){
-            for(int i = n-1;i>=0;i--){
-                ans[j][n-1-i] = g[i][j];
+                else if(g[i][j] == '#'){
+                    ans[k][n-i-1] = g[i][j];
+                    k--;
+                }
             }
         }
         return ans;
