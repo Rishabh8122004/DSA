897d829be3feb47cc7195015329c9d78ff3cf23a
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-05-08 08:08:38 +0530

    Time: 12 ms (60.33%) | Memory: 225.8 MB (21.74%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.cpp
+++ 3981-jump-game-ix/jump-game-ix.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<int> maxValue(vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int,int>>mini(n);
+        vector<int>mini(n);
         vector<int>maxi = nums;
-        int m = nums[n-1],mi = n-1;
+        int m = nums[n-1];
         for(int i = n-1;i>=0;i--){
             if(m>nums[i]){
                 m = nums[i];
-                mi = i;
             }
-            mini[i] = {m,mi};
+            mini[i] = m;
         }
         m = nums[0];
         for(int i = 0;i<n;i++){
@@ -22,7 +21,7 @@ public:
         for(int i = n-1;i>=0;i--){
             m = maxi[i];
             if(i<n-1){
-                if(m > mini[i+1].first){
+                if(m > mini[i+1]){
                     m = nums[i+1];
                 }
             }
