dadf17654606d7ccb8e01993f1a4ae6c36f1e9d1
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-05-09 07:39:59 +0530

    Time: 15 ms (48.53%) | Memory: 17.9 MB (34.56%) - LeetSync

---
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    vector<vector<int>> rotateGrid(vector<vector<int>>& g, int k) {
+        int n = g.size();
+        int m = g[0].size();
+        int l = 0, r = m - 1, u = 0, d = n - 1;
+        while ((l <= r - 1) && (u <= d - 1)) {
+            deque<int> q;
+            // filling the queue
+            int i = l, j = u;
+
+            while (j < d) { // left
+                q.push_back(g[j][i]);
+                j++;
+            }
+
+            while (i < r) { // bottom
+                q.push_back(g[j][i]);
+                i++;
+            }
+
+            while (j > u) { // right
+                q.push_back(g[j][i]);
+                j--;
+            }
+
+            while (i > l) { // top
+                q.push_back(g[j][i]);
+                i--;
+            }
+            cout << q.size() << "  ";
+            // rotating
+            int K = k % q.size();
+            while (K--) {
+                q.push_front(q.back());
+                q.pop_back();
+            }
+            // filling the grid
+            i = l, j = u;
+
+            while (j < d) {
+                g[j][i] = q.front();
+                q.pop_front();
+                j++;
+            }
+
+            while (i < r) {
+                g[j][i] = q.front();
+                q.pop_front();
+                i++;
+            }
+
+            while (j > u) {
+                g[j][i] = q.front();
+                q.pop_front();
+                j--;
+            }
+
+            while (i > l) {
+                g[j][i] = q.front();
+                q.pop_front();
+                i--;
+            }
+            l++;
+            r--;
+            u++;
+            d--;
+        }
+        return g;
+    }
+};
