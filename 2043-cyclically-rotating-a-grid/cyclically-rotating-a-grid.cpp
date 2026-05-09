4f2f5bc50b3dd9fad505f29098378680bc02cb7e
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-05-09 07:41:09 +0530

    Time: 16 ms (45.59%) | Memory: 17.4 MB (58.82%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
@@ -3,11 +3,11 @@ public:
     vector<vector<int>> rotateGrid(vector<vector<int>>& g, int k) {
         int n = g.size();
         int m = g[0].size();
-        int l = 0, r = m - 1, u = 0, d = n - 1;
+        int l = 0, r = m - 1, u = 0, d = n - 1,i,j,K;
+        deque<int> q;
         while ((l <= r - 1) && (u <= d - 1)) {
-            deque<int> q;
             // filling the queue
-            int i = l, j = u;
+            i = l; j = u;
 
             while (j < d) { // left
                 q.push_back(g[j][i]);
@@ -29,7 +29,7 @@ public:
                 i--;
             }
             // rotating
-            int K = k % q.size();
+            K = k % q.size();
             while (K--) {
                 q.push_front(q.back());
                 q.pop_back();
