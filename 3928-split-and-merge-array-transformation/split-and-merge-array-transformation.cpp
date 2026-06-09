77e369e4e620ef85d26021157b855595bd1f4200
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-06-09 08:34:06 +0530

    Time: 967 ms (25.71%) | Memory: 153.6 MB (55.24%) - LeetSync

---
+++ 3928-split-and-merge-array-transformation/split-and-merge-array-transformation.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    queue<vector<int>> q;
+    map<vector<int>, int> mp;
+    int solve(vector<int>& n2, int& n, int op) {
+        auto& v = q.front();
+        op = mp[v]; // number of operation till now..
+        if (v == n2) {
+            return mp[v];
+        }
+        for (int i = 0; i < v.size(); i++) {
+            for (int j = i; j < v.size(); j++) {
+                vector<int> block, rem, next;
+                for (int k = i; k <= j; k++)
+                    block.push_back(v[k]); // getting the block of the vector
+
+                // getting them remaining portion of the vector
+                for (int k = 0; k < i; k++)
+                    rem.push_back(v[k]); // prefix portion
+                for (int k = j + 1; k < n; k++)
+                    rem.push_back(v[k]); // suffix portion
+
+                // now inserting the block at every possible position in the
+                // remaining vector
+
+                // inserting at the front
+                next = block;
+                for (int k = 0; k < rem.size(); k++)
+                    next.push_back(rem[k]);
+                if (mp.count(next) == 0) {
+                    q.push(next);
+                    mp[next] = op + 1;
+                }
+                // inserting in the middle
+                int k = 0;
+                while (k < rem.size()) {
+                    next = {};
+                    for (int l = 0; l <= k; l++) {
+                        next.push_back(rem[l]);
+                    }
+                    for (int l = 0; l < block.size(); l++)
+                        next.push_back(block[l]);
+                    for (int l = k + 1; l < rem.size(); l++) {
+                        next.push_back(rem[l]);
+                    }
+                    if (mp.count(next) == 0) {
+                        q.push(next);
+                        mp[next] = op + 1;
+                    }
+                    k++;
+                }
+
+                // inserting at the end
+
+                next = {};
+                next = rem;
+                for(int k = 0;k<block.size();k++){
+                    next.push_back(block[k]);
+                }
+                if (mp.count(next) == 0) {
+                        q.push(next);
+                        mp[next] = op + 1;
+                }
+            }
+        }
+        q.pop();
+        return solve(n2,n,op);
+    }
+    int minSplitMerge(vector<int>& n1, vector<int>& n2) {
+        int n = n1.size();
+        mp[n1] = 0;
+        q.push(n1);
+        return solve(n2, n, 0); // n2 vector, size, number of operation
+    }
+};
