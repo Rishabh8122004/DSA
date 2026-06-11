856e39d757e7c2c761810d51eb0a2d6d42c1447b
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-06-11 23:20:59 +0530

    Time: 401 ms (33.56%) | Memory: 402.8 MB (12.75%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int max_depth = 0;
+    class Node {
+    public:
+        int val;
+        vector<Node*>v;
+        Node*parent = NULL;
+        Node(int val){
+            this->val = val;
+        }
+    };
+    int depth(Node*root){
+        if(root == NULL)return 0;
+        int ans = 0;
+        for(Node* node:root->v){
+            ans = max(ans,depth(node));
+        }
+        return ans+1;
+    }
+    int assignEdgeWeights(vector<vector<int>>& e) {
+        unordered_map<int,Node*>mp;
+        int p,c;
+        Node *par,*child;
+        for(int i = 0;i<e.size();i++){
+            p = e[i][0];
+            c = e[i][1];
+            if(!mp.count(p)){
+                par = new Node(p);
+                mp[p] = par;
+            }
+            if(!mp.count(c)){
+                child = new Node(c);
+                mp[c] = child;
+            }
+            par = mp[p];
+            child = mp[c];
+            par->v.push_back(child);
+            child->parent = par;
+        }
+        Node*root = mp[e[0][0]];
+        while(root->parent){
+            root = root->parent;
+        }
+        max_depth = depth(root)-2;
+        int ans = 1;
+        while(max_depth--){
+            ans*=2;
+            ans%=(1000000007);
+        }
+        return ans;
+     }
+};
