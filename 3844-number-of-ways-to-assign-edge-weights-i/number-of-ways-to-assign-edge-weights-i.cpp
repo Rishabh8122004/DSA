3bae4f3130ea8a428183ff95f0c79dbb204999c1
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-06-11 23:28:12 +0530

    Time: 394 ms (33.56%) | Memory: 402.7 MB (12.75%) - LeetSync

--- 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
@@ -4,32 +4,31 @@ public:
     class Node {
     public:
         int val;
-        vector<Node*>v;
-        Node*parent = NULL;
-        Node(int val){
-            this->val = val;
-        }
+        vector<Node*> v;
+        Node* parent = NULL;
+        Node(int val) { this->val = val; }
     };
-    int depth(Node*root){
-        if(root == NULL)return 0;
+    int depth(Node* root) {
+        if (root == NULL)
+            return 0;
         int ans = 0;
-        for(Node* node:root->v){
-            ans = max(ans,depth(node));
+        for (Node* node : root->v) {
+            ans = max(ans, depth(node));
         }
-        return ans+1;
+        return ans + 1;
     }
     int assignEdgeWeights(vector<vector<int>>& e) {
-        unordered_map<int,Node*>mp;
-        int p,c;
-        Node *par,*child;
-        for(int i = 0;i<e.size();i++){
+        unordered_map<int, Node*> mp;
+        int p, c;
+        Node *par, *child;
+        for (int i = 0; i < e.size(); i++) {
             p = e[i][0];
             c = e[i][1];
-            if(!mp.count(p)){
+            if (!mp.count(p)) {
                 par = new Node(p);
                 mp[p] = par;
             }
-            if(!mp.count(c)){
+            if (!mp.count(c)) {
                 child = new Node(c);
                 mp[c] = child;
             }
@@ -38,16 +37,23 @@ public:
             par->v.push_back(child);
             child->parent = par;
         }
-        Node*root = mp[e[0][0]];
-        while(root->parent){
+        Node* root = mp[e[0][0]];
+        while (root->parent) {
             root = root->parent;
         }
-        max_depth = depth(root)-2;
-        int ans = 1;
-        while(max_depth--){
-            ans*=2;
-            ans%=(1000000007);
+        max_depth = depth(root) - 2;
+        long long ans = 1, base = 2;
+        const long long MOD = 1e9 + 7;
+
+        while (max_depth) {
+            if (max_depth & 1) {
+                ans = (ans * base) % MOD;
+            }
+
+            base = (base * base) % MOD;
+            max_depth >>= 1;
         }
-        return ans;
-     }
+
+        return (int)ans;
+    }
 };
