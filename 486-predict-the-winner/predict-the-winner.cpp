dd95c3fa3e1bbe6287987074d0b593bbf2188786
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-08-01 09:40:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool solve(vector<int>&nums,int i,int j,long long p1,long long p2,bool f){
         if(i>j)return p1>=p2;
-        if(f){
+        if(f){ // player 1 is trying it's best 
             if(solve(nums,i+1,j,p1+nums[i],p2,false))return true;
             return solve(nums,i,j-1,p1+nums[j],p2,false);
         }
-        if(!solve(nums,i+1,j,p1,p2+nums[i],true))return false;
+        if(!solve(nums,i+1,j,p1,p2+nums[i],true))return false; // player 2 is trying its best
         return solve(nums,i,j-1,p1,p2+nums[j],true);
     }
     bool predictTheWinner(vector<int>& nums) {
