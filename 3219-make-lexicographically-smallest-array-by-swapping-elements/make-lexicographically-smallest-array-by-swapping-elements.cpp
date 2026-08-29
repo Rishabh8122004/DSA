cbb192ac7626366f015be317cabdb95b12957db6
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-08-29 10:08:49 +0530

    Time: 532 ms (11.55%) | Memory: 279.7 MB (14.36%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -8,14 +8,14 @@ public:
         }
         sort(v.begin(),v.end());
         unordered_map<int, priority_queue<int>*>mp;// every index in this map points to it's dedicated vector of elements that can be swaped with it
-        priority_queue<int>*pq = new(priority_queue<int>);
+        priority_queue<int>*pq = new priority_queue<int>;
         pq->push(v[0][0]);
         mp[v[0][1]] = pq;
         for(int i = 1;i<n;i++){
             if(abs(v[i][0]-v[i-1][0]) <= limit){
                 pq->push(v[i][0]);
             }else{
-                pq = new(priority_queue<int>);
+                pq = new  priority_queue<int>;
                 pq->push(v[i][0]);
             }
             mp[v[i][1]] = pq;
