b3638659588c3085a86769cda7d864a327132d6d
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-09-03 22:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 165.8 MB (62.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -7,14 +7,12 @@ public:
             x = min(x,nums[i]);
         }
         if(x%2 == 0){ // smallest is even i.e. we can just iterate the array to check if there is no odd
-            bool f = true;
             for(int i = 0;i<n;i++){
                 if(nums[i]%2 == 1){
-                    f = false;
-                    break;
+                    return false;
                 }
             } 
-            if(f)return true;
+            return true;
         }
         else{ // smallest is odd i.e. if there exist any even , we can make that odd so just return true;
         return true;
