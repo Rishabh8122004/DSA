734685613c10204a5ce7bc771772af505db48956
rishabh_08 <153280975+Rishabh8122004@users.noreply.github.com>
2026-09-25 17:59:09 +0530

    Time: 10 ms (54.02%) | Memory: 18.4 MB (12.95%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.cpp
+++ 1188-brace-expansion-ii/brace-expansion-ii.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     vector<string> braceExpansionII(string e) {
-
         int n = e.size();
-
         vector<string> merge;
         vector<string> concatenate;
-
-        // --------------------------------------------------
         // STEP 1: Find whether ROOT is OR
-        // --------------------------------------------------
-
         int depth = 0;
         int start = 0;
-
         for (int i = 0; i < n; i++) {
 
             if (e[i] == '{') {
@@ -30,7 +23,6 @@ public:
                 start = i + 1;
             }
         }
-
         // If we found a root-level comma,
         // complete the last OR child.
         if (!merge.empty()) {
@@ -38,51 +30,31 @@ public:
             merge.push_back(e.substr(start));
 
         }
-
-        // --------------------------------------------------
         // STEP 2: If no OR, find root-level AND pieces
-        // --------------------------------------------------
-
         else {
-
             int i = 0;
-
             while (i < n) {
-
                 // A brace expression
                 if (e[i] == '{') {
-
                     int j = i;
                     int depth = 1;
-
                     while (j + 1 < n && depth > 0) {
-
                         j++;
-
                         if (e[j] == '{')
                             depth++;
-
                         else if (e[j] == '}')
                             depth--;
                     }
-
                     // Remove outer { }
-                    concatenate.push_back(
-                        e.substr(i + 1, j - i - 1)
-                    );
-
+                    concatenate.push_back(e.substr(i + 1, j - i - 1));
                     i = j + 1;
                 }
-
                 // A normal lowercase string
                 else {
-
                     int j = i;
-
                     while (j < n && e[j] != '{') {
                         j++;
                     }
-
                     concatenate.push_back(
                         e.substr(i, j - i)
                     );
@@ -91,19 +63,11 @@ public:
                 }
             }
         }
-
-        // --------------------------------------------------
         // STEP 3: Evaluate ROOT = OR
-        // --------------------------------------------------
-
         vector<string> ans;
-
         if (!merge.empty()) {
-
             unordered_set<string> st;
-
             for (auto& s : merge) {
-
                 vector<string> temp =
                     braceExpansionII(s);
 
@@ -111,58 +75,37 @@ public:
                     st.insert(p);
                 }
             }
-
             for (auto& p : st) {
                 ans.push_back(p);
             }
         }
-
-        // --------------------------------------------------
         // STEP 4: Evaluate ROOT = AND
-        // --------------------------------------------------
-
         else {
-
             unordered_set<string> st;
-
             // Literal expression
             if (concatenate.size() == 1 &&
                 concatenate[0] == e) {
-
                 return {e};
             }
-
-            vector<string> first =
-                braceExpansionII(concatenate[0]);
-
+            vector<string> first =  braceExpansionII(concatenate[0]);
             for (auto& p : first) {
                 st.insert(p);
             }
-
             for (int i = 1; i < concatenate.size(); i++) {
-
-                vector<string> temp =
-                    braceExpansionII(concatenate[i]);
-
+                vector<string> temp = braceExpansionII(concatenate[i]);
                 unordered_set<string> next;
-
                 for (auto& p : st) {
                     for (auto& q : temp) {
-
                         next.insert(p + q);
                     }
                 }
-
                 st = next;
             }
-
             for (auto& p : st) {
                 ans.push_back(p);
             }
         }
-
         sort(ans.begin(), ans.end());
-
         return ans;
     }
 };
